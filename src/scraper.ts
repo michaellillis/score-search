e3c3621fb517ebc7a0fdf0e1b69eadc4a693634e
TylerJenningsW <tylerjennings2@outlook.com>
2022-07-05 21:48:48 -0500

    feature=web-scraper Changed scraper to take input, click container, and screenshot

--- src/scraper.ts
+++ src/scraper.ts
@@ -1,24 +1,30 @@
 import * as puppeteer from 'puppeteer';
 
-let browser: any;
-(async () => {
-  const searchQuery = "76ers stats";
-
-  browser = await puppeteer.launch();
-  const [page] = await browser.pages();
-  await page.goto("https://www.google.com/", {waitUntil: "domcontentloaded"});
-  await page.waitForSelector('input[aria-label="Search"]', {visible: true});
-  await page.type('input[aria-label="Search"]', searchQuery);
-  await Promise.all([
-    page.waitForNavigation(),
-    page.keyboard.press("Enter"),
-  ]);
-  await page.waitForSelector(".LC20lb", {visible: true});
-  const searchResults = await page.$$eval(".LC20lb", (els: any) =>
-  els.map((e: any) => ({title: e.innerText, link: e.parentNode.href}))
-  );
-  console.log(searchResults);
-})()
-  .catch(err => console.error(err))
-  .finally(() => browser?.close())
-;
+async function scrape(input: string): Promise<void> {
+    let browser: puppeteer.Browser;
+    (async () => {
+      const searchQuery = input;
+    
+      browser = await puppeteer.launch();
+      const [page] = await browser.pages();
+      await page.goto("https://www.google.com/", {waitUntil: "domcontentloaded"});
+      await page.waitForSelector('input[aria-label="Search"]', {visible: true});
+      await page.type('input[aria-label="Search"]', searchQuery);
+      await Promise.all([
+        page.waitForNavigation(),
+        page.keyboard.press("Enter"),
+      ]);
+      const [button] = await page.$x("/html/body/div[7]/div/div[10]/div[1]/div[2]/div[2]/div/div/div[1]/block-component/div/div[1]/div/div/div/div[1]/div/div/div/div/div/div/div[3]");
+      if (button) {
+          await button.click();
+          window.setTimeout(() => 5000);
+          await page.screenshot({ 
+            path: "./screenshot.png",
+            fullPage: true         
+          });
+      }
+    })()
+      .catch(err => console.error(err))
+      .finally(() => browser?.close())
+    ;
+}
