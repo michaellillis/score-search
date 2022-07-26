23b240152b607597458e7bbcb79eda54e539e19a
TylerJenningsW <tylerjennings2@outlook.com>
2022-07-26 18:18:46 -0500

    Changed combine function to delimit illegal filename characters

--- src/utils/joinWords.ts
+++ src/utils/joinWords.ts
@@ -1,7 +1,7 @@
 export function combine(input: string) {
   const join = input
     .toLowerCase()
-    .split(' ')
+    .split(/[ .:;?!~,`"&|()<>{}\[\]\r\n/\\]+/)
     .map((s) => s.charAt(0).toUpperCase() + s.substring(1))
     .join('');
   return join;
